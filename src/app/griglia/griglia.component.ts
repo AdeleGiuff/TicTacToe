cde269501231e9d7f0fe8948f8175b3754b6828a
Adele Giuffre <jadelle.giu@gmail.com>
2021-11-16 10:16:41 +0100

    modifiche con aggiunta di changedetection e eventemitter nel quadrato component

--- src/app/griglia/griglia.component.ts
+++ src/app/griglia/griglia.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-griglia',
   templateUrl: './griglia.component.html',
   styleUrls: ['./griglia.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GrigliaComponent implements OnInit {
   // i quadrati, cioè le nove mosse sono un array di stringhe.
   quadrati!: any[];
-  giocatoreAttuale!: boolean;
+  giocatoreAttuale: 'X' | 'O' | null = 'X';
   vincitore!: string | null;
-  constructor() {}
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.nuovaPartita();
@@ -20,25 +27,31 @@ export class GrigliaComponent implements OnInit {
     //per la proprietà quadrati creiamo un array di 9 elementi al quale inizialmente diamo il valore null.
     this.quadrati = Array(9).fill(null);
     // quando la proprietà giocatore attuale è settata in true il vincitore ha valore null.
-    this.giocatoreAttuale = true;
+    this.giocatoreAttuale = 'X'; //true == X, false == O
     this.vincitore = null;
   }
   //con il getter gestiamo la logica della UI del pulsante giocatore
-  get giocatore() {
-    //quando la proprietà del giocatoreAttuale è true il giocatore sarà X, altrimenti O.
-    //Tutte le volte che cambia il valore di giocatoreAttuale cambia la UI della proprietà giocatore.
-    return this.giocatoreAttuale ? 'X' : 'O';
-  }
-  faiUnaMossa(index: number) {
+  // get giocatore() {
+  //   //quando la proprietà del giocatoreAttuale è true il giocatore sarà X, altrimenti O.
+  //   //Tutte le volte che cambia il valore di giocatoreAttuale cambia la UI della proprietà giocatore.
+  //   return this.giocatoreAttuale ? 'X' : 'O';
+  // }
+  faiUnaMossa(index: number, event: string) {
+    console.log(event);
     // se i quadrati sono già stati cliccati non sarà possibile ricliccarli.
     //Se i quadrati sono vuoti, utilizzando il metodo splice tagliamo l'array dalla posizione del quadrato su cui abbiamo cliccato
     //per cambiare la UI del giocatore settiamo il giocatoreAttuale al valore opposto.
-    if (!this.quadrati[index]) {
-      this.quadrati.splice(index, 1, this.giocatore);
-      this.giocatoreAttuale = !this.giocatoreAttuale;
-    }
-
-    this.vincitore = this.calcolaVincitore();
+    setTimeout(() => {
+      if (!this.quadrati[index]) {
+        this.quadrati = this.quadrati.map((x, i) =>
+          i === index ? this.giocatoreAttuale : x
+        );
+        //this.quadrati.splice(index, 1, this.giocatoreAttuale);
+        this.giocatoreAttuale = this.giocatoreAttuale === 'X' ? 'O' : 'X';
+      }
+      this.changeDetectorRef.detectChanges();
+      this.vincitore = this.calcolaVincitore();
+    }, 0);
   }
   calcolaVincitore() {
     //algoritmo su ogni combinazione di possibili vincite. Ci sono solo 9 mosse per cui solo 8 possibilità di vincere.
--- src/app/quadrato/quadrato.component.ts
+++ src/app/quadrato/quadrato.component.ts
@@ -1,12 +1,19 @@
 //inlineTemplate e inlineStyle component
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quadrato',
-  template: ` <button class="pulsante">{{ valore }}</button> `,
+  template: `<div class="pulsante" style="border: 1px solid red;">
+    {{ valore }} <button (click)="buttonClick()">Play</button>
+  </div>`,
 })
 export class QuadratoComponent {
   //attraverso l'uso della keyword input permettiamo che la proprietà sia inserita nel componente.
   //Quando una componente ha un solo input viene chiamata componente UI
   @Input() valore!: 'X' | 'O';
+  @Output() onPlay = new EventEmitter<string>();
+
+  buttonClick() {
+    this.onPlay.emit(this.valore);
+  }
 }
